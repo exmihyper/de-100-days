e4ad7e7576ba227ff572ef46df3b44b5d5352831
Nikita Nikitin <hailtheeternity@gmail.com>
2026-07-27 18:11:46 +0300

    Day 8 (27.07.2026): SELECT, WHERE, ORDER BY, DISTINCT, COUNT basics

--- week-02-sql/day-08/queries.sql
+++ week-02-sql/day-08/queries.sql
@@ -1,20 +1,29 @@
+-- Задача 1
+-- Выведи всех сотрудников из отдела Finance с зарплатой больше 100000.
 SELECT name
 FROM employees
-WHERE department = 'Finance'
-AND salary > 100000
-;
+WHERE department = 'Finance' AND salary > 100000;
 
+
+
+-- Задача 2
+-- Выведи названия продуктов из таблицы orders, у которых amount > 10000.
 SELECT DISTINCT product
 FROM orders
-WHERE amount > 10000
-;
+WHERE amount > 10000;
 
+
+
+-- Задача 3
+-- Выведи имена и даты найма сотрудников, нанятых раньше 2019 года, отсортированных по дате найма (сначала самые старые).
 SELECT name, hire_date
 FROM employees
 WHERE hire_date < '2019-01-01'
-ORDER BY hire_date ASC
-;
+ORDER BY hire_date;
 
+
+
+-- Задача 4
+-- Посчитай количество строк в таблице orders.
 SELECT COUNT(*)
-FROM orders
-;
+FROM orders;
